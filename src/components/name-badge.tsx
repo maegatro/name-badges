aa405e8ec9988eba91b2618fcdbe7f573d6c368c
maegatro <shuntaromaekawa@gmail.com>
2023-09-14 17:18:32 +0900

    folder changes

--- src/components/name-badge.tsx
+++ src/components/name-badge.tsx
@@ -1,12 +1,12 @@
-const NameBadge = () => {
+const NameBadge = ({ greeting, name }: { greeting: string; name: string }) => {
   return (
     <section className="badge">
       <header className="badge-header">
-        <h1 className="text-5xl">HELLO</h1>
+        <h1 className="text-5xl">{greeting}</h1>
         <p>My name is…</p>
       </header>
       <div className="badge-body">
-        <p className="badge-name">Steve</p>
+        <p className="badge-name">{name}</p>
       </div>
       <footer className="badge-footer" />
     </section>
